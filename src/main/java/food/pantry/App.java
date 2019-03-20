4d6520fc8a034a03a1dbd5ff781ab821fe5fa91b
jfarrar1 <46975380+jfarrar1@users.noreply.github.com>
2019-03-20 15:31:28 -0400

    Update App.java

--- src/main/java/food/pantry/App.java
+++ src/main/java/food/pantry/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) throws Exception {
-    	//SpringApplication.run(App.class, args);
-        FoodKeeperStorage.storeFoodKeeperData();
+    	SpringApplication.run(App.class, args);
     }
 }
